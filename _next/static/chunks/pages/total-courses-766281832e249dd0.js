b1acebab95895e17dfc476405254003efc7b4361
Emartnz <126187346+Emartnz@users.noreply.github.com>
2023-03-01 03:09:10 -0600

    Add files via upload

---
+++ _next/static/chunks/pages/total-courses-766281832e249dd0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[359],{2785:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/total-courses",function(){return u(7004)}])}},function(n){n.O(0,[90,237,865,893,4,774,888,179],function(){return n(n.s=2785)}),_N_E=n.O()}]);
